b63f65852e92e61081d930bb009320140a75b9aa
Wagh Pooja Maruti <91519272+Wagh-Pooja@users.noreply.github.com>
2022-06-26 21:37:15 +0530

    LB Assignment

---
+++ LB Assignment 01/02 Program To Print 5 Times 'Marvellous' On Screen.c
@@ -0,0 +1,19 @@
+#include<conio.h>
+#include<stdio.h>
+#include<stdlib.h>
+
+void Display()
+{
+    int i = 0;
+
+    for(i = 1; i<=5 ;i++)
+    {
+        printf("\nMarvellous\n");
+    }
+}
+
+int main()
+{
+    Display();
+    return 0;
+}
