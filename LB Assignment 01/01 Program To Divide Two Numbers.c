17fb0e558abe2af20561e5aa854310ea94f111e2
Wagh Pooja Maruti <91519272+Wagh-Pooja@users.noreply.github.com>
2022-06-26 21:36:48 +0530

    LB Assignment

---
+++ LB Assignment 01/01 Program To Divide Two Numbers.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+
+int Divide(int iNo1, int iNo2)
+{
+    int iAns = 0;
+
+    if(iNo2 == 0)
+    {
+        return -1;
+    }
+
+    iAns = iNo1 / iNo2;
+
+    return iAns;
+}
+
+int main()
+{
+    int iValue1 = 15, iValue2 = 5;
+    int iRet = 0;
+
+    iRet = Divide(iValue1, iValue2);
+
+    printf("\nDivision of Two Numbers is = %d",iRet);
+
+    return 0;
+}
