eac87996d8c845f3ddcb494173737157c98c4ea3
Pooja Maruti Wagh <91519272+Wagh-Pooja@users.noreply.github.com>
2022-07-05 22:01:35 +0530

    LB Assignment

---
+++ LB Assignment 03/02 Write a program which accept number from user and print even factors of that number.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+
+4. Accept one character from user and convert case of that character.
+Input : a Output : A
+Input : D Output : d
+______ DisplayConvert ( _____ CValue)
+{
+if(______)
+{
+    printf(“% ___” , _____);
+}
+else if(_____)
+{
+printf(“% ___” , _____);
+}
+}
+int main()
+{
+char cValue = ‘\0’;
+printf(“Enter character\n”);
+scanf(“%c”,&cValue);
+_________ (cValue );
+return 0;
+}
