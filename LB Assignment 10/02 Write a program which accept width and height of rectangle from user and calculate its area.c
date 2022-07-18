811315f49f062912c669672160e6c2c2268faa4c
Pooja Maruti Wagh <91519272+Wagh-Pooja@users.noreply.github.com>
2022-07-18 22:40:28 +0530

    LB Assignment

---
+++ LB Assignment 10/02 Write a program which accept width and height of rectangle from user and calculate its area.c
@@ -0,0 +1,32 @@
+///Write a program which accept width & height of rectangle from user and calculate its area(Area = Width * Height)
+///Input : 5.3 9.78
+///Output : 51.834
+
+#include<stdio.h>
+
+double RectArea(float fWidth, float fHeight)
+{
+    float Area = 0.0;
+
+    Area = fWidth*fHeight;
+
+    return Area;
+}
+
+int main()
+{
+    float fValue1 = 0.0, fValue2 = 0.0;
+    double dRet = 0.0;
+
+    printf("Enter Width = ");
+    scanf("%f",&fValue1);
+
+    printf("Enter Height =");
+    scanf("%f",&fValue2);
+
+    dRet = RectArea(fValue1, fValue2);
+
+    printf("\nArea of Rectangle is %f",dRet);
+
+    return 0;
+}
